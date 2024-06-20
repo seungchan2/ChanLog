a1df17c3de897e65e4188c4a00315a6616c569bf
seungchan2 <chanchu@hansung.ac.kr>
2024-06-20 13:07:17 +0900

    [1.0.1] edit minimun target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "ChanLog",
     platforms: [
          // Specify the minimum version of iOS required.
-         .iOS(.v14)
+         .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
